01681c8f381a9e66ac9275021ebccd1f8bd8115e
willyserge <willyserge01@gmail.com>
2020-06-17 13:46:42 +0200

    test socket.io connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 
 import history from './history';
 import GameMenu from './components/GameMenu';
+import socket from './socketConfig';
 
 function App() {
+  useEffect(() => {
+    socket.on('test', (msg) => {
+      console.log(msg);
+    });
+  }, []);
   return (
     <Router history={history}>
       <Switch>
