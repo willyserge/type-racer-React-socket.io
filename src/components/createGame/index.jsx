05e8d3b60d5c46229a55897429be0dcf4e8c9192
willyserge <willyserge01@gmail.com>
2020-06-22 18:32:36 +0200

    create new game

---
+++ src/components/createGame/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {
+  Container, Header, Form, Button,
+} from 'semantic-ui-react';
+import socket from '../../socketConfig';
+
+function CreateGame(props) {
+  const [name, setName] = useState('');
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+    socket.emit('create-game', name);
+  }
+  return (
+    <Container text textAlign="center">
+      <Header as="h1">Create Game</Header>
+
+      <Form onSubmit={onSubmit}>
+
+        <Form.Input
+          fluid
+          label="Nickname"
+          placeholder="Enter Nickname"
+          name="nickname"
+          type="text"
+          onChange={handleChange}
+          value={name}
+        />
+
+        <Button
+          type="submit"
+          content="Create Game"
+          color="blue"
+        />
+
+      </Form>
+    </Container>
+  );
+}
+
+export default CreateGame;
